088ef79d6afd35702572e1c6eb5a7d43651fe51f
jholopai <jholopai@users.noreply.github.com>
2023-08-04 15:02:40 +0300

    1.8: unicafe, step 3

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -4,16 +4,27 @@ const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 );
 
+const Statistics = (props) => {
+  const feedbackTotal = props.good + props.neutral + props.bad;
+  const feedbackAverage = (props.good - props.bad) / feedbackTotal || 0;
+  const feedbackPositive = (100 / feedbackTotal) * props.good || 0;
+
+  return (
+    <div>
+      <p>good: {props.good}</p>
+      <p> neutral: {props.neutral} </p>
+      <p>bad: {props.bad}</p>
+      <p>total: {feedbackTotal}</p>
+      <p>average: {feedbackAverage}</p>
+      <p>positive: {feedbackPositive}%</p>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const feedbackTotal = good + neutral + bad;
-
-  const feedbackAverage = (good - bad) / feedbackTotal || 0;
-
-  const feedbackPositive = (100 / feedbackTotal) * good || 0;
 
   return (
     <div>
@@ -21,12 +32,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <p>good: {good}</p>
-      <p> neutral: {neutral} </p>
-      <p>bad: {bad}</p>
-      <p>total: {feedbackTotal}</p>
-      <p>average: {feedbackAverage}</p>
-      <p>positive: {feedbackPositive}%</p>
+      <Statistics good={good} bad={bad} neutral={neutral}></Statistics>
     </div>
   );
 };
